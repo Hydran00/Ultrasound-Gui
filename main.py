505094e494ba892936523abd835c8739a549aef0
Hydran00 <davide.nardi-1@studenti.unitn.it>
2024-02-18 16:48:52 +0100

    Minor

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import sys,os,signal
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
-from PyQt5.QtCore import QProcess, pyqtSlot
-# font
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QTextEdit
+from PyQt5.QtCore import Qt, QProcess, pyqtSlot
 from PyQt5.QtGui import QFont
 import utils
+
 class SubprocessButton(QPushButton):
-    def __init__(self, command, label, parent=None):
+    def __init__(self, command, label, output_widget, parent=None):
         super().__init__(parent)
         self.label = label
-        self.setFixedSize(300, 50)
+        self.output_widget = output_widget
+        self.setFixedSize(500, 200)
         self.setText("Start " + self.label)
-        self.setFont(QFont('Arial', 15)) 
+        self.setFont(QFont('Arial', 18)) 
         self.command = command
         self.process = QProcess()
         self.running = False
@@ -31,17 +32,25 @@ class SubprocessButton(QPushButton):
             self.setText(self.label + " running")           
             self.process.start(self.command)
             print(self.command)
-            # print(f"Started {self.label}")
+            self.process.readyReadStandardOutput.connect(self.read_output)
+            self.process.readyReadStandardError.connect(self.read_output)
             self.process.finished.connect(self.on_finished)
             
     def kill_subprocess(self):
         if self.running:
-            self.setText(self.label + " terminating")
+            self.setText("Terminating")
             killer = QProcess()
             print("Killing PID", self.process.processId())
             # os.kill(self.process.processId(), signal.SIGINT)
             killer.start("kill -SIGINT " + str(self.process.processId()))
             killed = killer.waitForFinished(1000)
+    @pyqtSlot()
+    def read_output(self):
+        output = self.process.readAllStandardOutput().data().decode()
+        error = self.process.readAllStandardError().data().decode()
+        output_text = output + error
+        self.output_widget.append(output_text)
+
     @pyqtSlot(int, QProcess.ExitStatus)
     def on_finished(self, exitCode, exitStatus):
         print(f"{self.label} finished with code {exitCode}")
@@ -53,24 +62,24 @@ class SubprocessButton(QPushButton):
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Subprocess Controller")
+        self.setWindowTitle("Control Panel")
         layout = QGridLayout()
         self.setLayout(layout)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
         
-        # read cmds and labels from file
+        # Dictionary to hold buttons and their associated text boxes
+        self.button_textbox_map = {}
+
+        # add buttons
         commands = utils.read_from_file("commands.txt")
         labels = utils.read_from_file("labels.txt")
-        if(len(commands) != len(labels)):
-            print("Error: commands and labels have different lengths")
-            sys.exit(1)
-        # add buttons
-        print("Commands is ",commands)
-        for button in range(len(commands)):
-            print("Creating button for")
-            print(commands[button])
-            print(labels[button])
-            button_launch = SubprocessButton(commands[button], labels[button])
-            layout.addWidget(button_launch, 0, button)
+        for i, command in enumerate(commands):
+            output_textbox = QTextEdit()
+            output_textbox.setMinimumWidth(500)
+            layout.addWidget(output_textbox, i, 1)
+            button_launch = SubprocessButton(command, labels[i], output_textbox)
+            layout.addWidget(button_launch, i, 0)
+            self.button_textbox_map[button_launch] = output_textbox
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
