c7ce0d24f42f92dd43604f94e847bb975141b34a
Nardi <davide.nardi-1@studenti.unitn.it>
2024-05-29 09:02:36 +0200

    Minorg

--- ros_source.sh
+++ ros_source.sh
@@ -2,28 +2,30 @@
 # check if the paths are not in $COLCON_PREFIX_PATH
 
 # ROS2
-if [[ ":$COLCON_PREFIX_PATH:" != *":/opt/ros/humble:"* ]]; then
-  source /opt/ros/humble/setup.bash
-fi
+source /opt/ros/humble/setup.bash
 # ur workspace
 if [[ ":$COLCON_PREFIX_PATH:" != *":/home/nardi/Ultrasound-Demo/robot_ws/ur_ws/install:"* ]]; then
   source /home/nardi/Ultrasound-Demo/robot_ws/ur_ws/install/setup.bash
+  echo "ur_ws sourced"
 fi
 # controllers
 if [[ ":$COLCON_PREFIX_PATH:" != *":/home/nardi/Ultrasound-Demo/robot_ws/controller_ws/install:"* ]]; then
   source /home/nardi/Ultrasound-Demo/robot_ws/controller_ws/install/setup.bash
+  echo "controller_ws sourced"
 fi
 # force torque sensor
 if [[ ":$COLCON_PREFIX_PATH:" != *":/home/nardi/Ultrasound-Demo/robot_ws/force_torque_sensor_ws/install:"* ]]; then
   source /home/nardi/Ultrasound-Demo/robot_ws/bota_ws/install/setup.bash
+  echo "force_torque_sensor_ws sourced"
 fi
 # ultrasound workspace
 if [[ ":$COLCON_PREFIX_PATH:" != *":/home/nardi/Ultrasound-Demo/ultrasound_ws/install:"* ]]; then
   source /home/nardi/Ultrasound-Demo/ultrasound_ws/install/setup.bash
+  echo "ultrasound_ws sourced"
 fi
 # haptic interface
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hydran00/haption_ws/src/haption_raptor_api/Dependencies/RaptorAPI/bin/Linux/glibc-2.35/
 if [[ ":$COLCON_PREFIX_PATH:" != *":/home/nardi/Ultrasound-Demo/haption_ws/install:"* ]]; then
     source /home/nardi/Ultrasound-Demo/haption_ws/install/setup.bash
+    echo "haption_ws sourced"
 fi
-
